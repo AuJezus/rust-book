25b5220c2a7e309ce8a41aedd9e9d3591d36a309
aujezus <augispay@gmail.com>
2024-03-19 19:01:18 +0200

    3.3 Functions

---
+++ variables/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // Mutable variables
+    // let mut x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is: {x}");
+
+    // Shadowing
+    // let x = 5;
+    // let x = x + 1;
+
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
+    // println!("The value of x is: {x}");
+
+    // let spaces = "    ";
+    // let spaces = spaces.len();
+    // println!("{spaces}");
+
+    let test: u8 = 257;
+    println!("{test}");
+}
